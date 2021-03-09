00494a88c0ca58c019ec409635bdb483c4cfa365
後雨 <1301749707@qq.com>
2021-03-09 15:38:09 +0800

    添加力扣206题相交链表

---
+++ LeetCode/ArrayAndList/List/ReverseList.java
@@ -0,0 +1,39 @@
+package ArrayAndList.List;
+
+/**
+ * LeetCode 206. 反转链表
+ */
+public class ReverseList {
+    /**
+     * 迭代法实现反转链表
+     * @param head
+     * @return
+     */
+    public ListNode reverseList1(ListNode head) {
+        if(head == null)
+            return null;
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    /**
+     * 递归法实现反转链表
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode newHead = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
