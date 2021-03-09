f5abf3d2f30a968605aaf9a581dcd3521daf4983
後雨 <1301749707@qq.com>
2021-03-09 15:37:52 +0800

    提出公用链表节点类

--- LeetCode/ArrayAndList/List/GetIntersectionNode.java
+++ LeetCode/ArrayAndList/List/GetIntersectionNode.java
@@ -3,14 +3,6 @@ package ArrayAndList.List;
 /**
  * LeetCode 160. 相交链表
  */
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
-}
 class GetIntersectionNode {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if(headA == null || headB == null) return null;
