f9d08559630aac3196ca8db1c76623ad77a4190c
ming <1301749707@qq.com>
2021-07-02 17:31:05 +0800

    435. 无重叠区间

--- LeetCode/greedy/EraseOverlapIntervals.java
+++ LeetCode/greedy/EraseOverlapIntervals.java
@@ -1,17 +1,15 @@
 package greedy;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
+/**
+ * 435. 无重叠区间
+ */
 public class EraseOverlapIntervals {
     public int eraseOverlapIntervals(int[][] intervals) {
         if(intervals == null || intervals.length == 0)
             return 0;
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a, int[] b) {
-                return a[1] - b[1];
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
         int cnt = 0;
         int prev = intervals[0][1];
         for (int i = 1; i < intervals.length; ++i) {
