f60ae8d6b727df2046f3630e9f2a3d9177bffb29
ming <1301749707@qq.com>
2021-07-02 17:16:18 +0800

    167. 两数之和 II - 输入有序数组

---
+++ LeetCode/twoPointers/TwoSumII.java
@@ -0,0 +1,30 @@
+package twoPointers;
+
+import java.util.Arrays;
+
+/**
+ * 167. 两数之和 II - 输入有序数组
+ */
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1;
+        while(start < end) {
+            int sum = numbers[start] + numbers[end];
+            if(sum < target)
+                ++start;
+            else if(sum > target)
+                --end;
+            else
+                break;
+        }
+        return new int[]{start+1, end+1};
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{2, 7, 11, 15};
+        int target = 9;
+        TwoSumII t = new TwoSumII();
+        int[] res = t.twoSum(numbers, target);
+        System.out.println(Arrays.toString(res));
+    }
+}
