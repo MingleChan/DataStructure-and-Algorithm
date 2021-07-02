c34d9ce793ec0433e9e30cbd7523d2375a320492
ming <1301749707@qq.com>
2021-07-02 17:38:17 +0800

    21. 合并两个有序链表

--- LeetCode/list/MergeTwoLists.java
+++ LeetCode/list/MergeTwoLists.java
@@ -1,22 +1,23 @@
 package list;
 
 /**
- * LeetCode 21. 合并两个有序链表
+ * 21. 合并两个有序链表
  */
 class MergeTwoLists {
     /**
      * 迭代法合并两个单链表
+     *
      * @param p1
      * @param p2
      * @return
      */
     public ListNode mergeTwoLists1(ListNode p1, ListNode p2) {
-        if(p1 == null) return p2;
-        if(p2 == null) return p1;
+        if (p1 == null) return p2;
+        if (p2 == null) return p1;
         ListNode prehead = new ListNode(-1);
         ListNode cur = prehead;
-        while(p1 != null && p2 != null){
-            if(p1.val < p2.val){
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
                 cur.next = p1;
                 p1 = p1.next;
             } else {
@@ -25,31 +26,56 @@ class MergeTwoLists {
             }
             cur = cur.next;
         }
-        cur.next = p1 == null ? p2:p1;
+        cur.next = p1 == null ? p2 : p1;
         return prehead.next;
     }
 
     /**
      * 递归法合并两个单链表
+     *
      * @param p1
      * @param p2
      * @return
      */
     public ListNode mergeTwoLists2(ListNode p1, ListNode p2) {
-        if(p1 == null)
+        if (p1 == null)
             return p2;
-        else if(p2 == null)
+        else if (p2 == null)
             return p1;
         else {
             ListNode head = null;
-            if(p1.val < p2.val){
+            if (p1.val < p2.val) {
                 head = p1;
                 head.next = mergeTwoLists2(p1.next, p2);
-            } else{
+            } else {
                 head = p2;
                 head.next = mergeTwoLists2(p1, p2.next);
             }
             return head;
         }
+    }
+
+    public static void main(String[] args) {
+        ListNode A3 = new ListNode(4);
+        ListNode A2 = new ListNode(2, A3);
+        ListNode A1 = new ListNode(1, A2);
+        ListNode B3 = new ListNode(4);
+        ListNode B2 = new ListNode(3, B3);
+        ListNode B1 = new ListNode(1, B2);
+
+        MergeTwoLists m = new MergeTwoLists();
+        ListNode cur = m.mergeTwoLists1(A1, B1);
+        StringBuilder sb = new StringBuilder("[");
+        while (cur != null) {
+            sb.append(cur.val);
+            sb.append(',');
+            cur = cur.next;
+        }
+        if (sb.length() == 1)
+            sb.append(']');
+        else
+            sb.setCharAt(sb.length()-1,']');
+        String res = sb.toString();
+        System.out.println(res);
     }
 }
