a0ef836c98f04c8d7ccf6a2b0023df2e276fbe5c
ming <1301749707@qq.com>
2021-07-02 17:02:33 +0800

    贪心算法

---
+++ LeetCode/greedy/ReconstructQueue.java
@@ -0,0 +1,30 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReconstructQueue {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (p1, p2) -> {
+            if(p1[0] != p2[0]) {
+                return p2[0] - p1[0];
+            } else {
+                return p1[1] - p2[1];
+            }
+        });
+        List<int[]> res = new LinkedList<>();
+        for(int[] p : people) {
+            res.add(p[1], p);
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        ReconstructQueue r = new ReconstructQueue();
+        int[][] res = r.reconstructQueue(people);
+        System.out.println(Arrays.deepToString(res));
+    }
+}
