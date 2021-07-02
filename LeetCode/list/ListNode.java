bb01491cae44f6e2303f233ff9accbea340ee880
ming <1301749707@qq.com>
2021-07-02 17:43:37 +0800

    ListNode

--- LeetCode/list/ListNode.java
+++ LeetCode/list/ListNode.java
@@ -9,4 +9,18 @@ public class ListNode {
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public static void printList(ListNode cur){
+        StringBuilder sb = new StringBuilder("[");
+        while (cur != null) {
+            sb.append(cur.val);
+            sb.append(',');
+            cur = cur.next;
+        }
+        if (sb.length() == 1)
+            sb.append(']');
+        else
+            sb.setCharAt(sb.length()-1,']');
+        String res = sb.toString();
+        System.out.println(res);
+    }
 }
