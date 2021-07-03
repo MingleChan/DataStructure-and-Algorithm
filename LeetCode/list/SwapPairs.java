cefc05a50fff432ba75b4b4d582191056d338589
ming <1301749707@qq.com>
2021-07-03 15:03:16 +0800

    24. 两两交换链表中的节点

--- LeetCode/list/SwapPairs.java
+++ LeetCode/list/SwapPairs.java
@@ -1,7 +1,7 @@
 package list;
 
 /**
- * LeetCode 24. 两两交换链表中的节点
+ * 24. 两两交换链表中的节点
  */
 class SwapPairs {
     public ListNode swapPairs(ListNode head) {
@@ -11,5 +11,16 @@ class SwapPairs {
         head.next = swapPairs(next.next);
         next.next = head;
         return next;
+    }
+
+    public static void main(String[] args) {
+        ListNode L5 = new ListNode(5);
+        ListNode L4 = new ListNode(4, L5);
+        ListNode L3 = new ListNode(3, L4);
+        ListNode L2 = new ListNode(2, L3);
+        ListNode L1 = new ListNode(1, L2);
+        SwapPairs s = new SwapPairs();
+        ListNode head = s.swapPairs(L1);
+        ListNode.printList(head);
     }
 }
