bd8262dae93c2cfc30239927807bf6e14006a5e8
ming <1301749707@qq.com>
2021-07-08 20:22:17 +0800

    4. 寻找两个正序数组的中位数

--- LeetCode/array/FindMedianSortedArrays.java
+++ LeetCode/array/FindMedianSortedArrays.java
@@ -4,7 +4,7 @@ package array;
  * 4. 寻找两个正序数组的中位数
  * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
  */
-public class FindMedianSortedArrays {
+class FindMedianSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len1 = nums1.length, len2 = nums2.length;
         int totalLen = len1 + len2;
